87cf6ceebe31bf30ed774b49ec1c9fcace73cebc
leandro0101 <leandrovieira3306@gmail.com>
2021-03-17 16:55:12 -0300

    feat: ensure AddUserService throw if user already exists

--- src/data/services/user/add-user.ts
+++ src/data/services/user/add-user.ts
@@ -1,11 +1,17 @@
 import { ICreateUserDTO, IReturnUserDTO } from '@domain/dtos'
-import { IAddUserUseCase } from '@domain/usecases'
+import { IAddUserUseCase, ILoadUserByEmailUseCase } from '@domain/usecases'
 import { IAddUserRepository } from '../../repositories/'
 
 export class AddUserService implements IAddUserUseCase {
-  constructor (private readonly addUserRepository: IAddUserRepository) {}
+  constructor (private readonly addUserRepository: IAddUserRepository, private readonly loadUserByEmailService: ILoadUserByEmailUseCase) {}
 
   async execute (userData: ICreateUserDTO): Promise<IReturnUserDTO> {
+    const userExists = await this.loadUserByEmailService.execute(userData.email)
+
+    if (userExists) {
+      throw new Error('User already exists')
+    }
+
     return await this.addUserRepository.execute(userData)
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
 import { connect } from './infra/typeorm/helpers/typeorm'
 import 'reflect-metadata'
-import { LoadUserByEmailService } from '@data/services'
-import { ILoadUserByEmailUseCase } from '@domain/usecases'
-import { ILoadUserByEmailRepository } from '@data/repositories'
-import { LoadUserByEmailRepository } from '@infra/repositories'
+import { AddUserService, LoadUserByEmailService } from '@data/services'
+import { IAddUserUseCase, ILoadUserByEmailUseCase } from '@domain/usecases'
+import { IAddUserRepository, ILoadUserByEmailRepository } from '@data/repositories'
+import { AddUserRepository, LoadUserByEmailRepository } from '@infra/repositories'
 
 connect().then(async () => {
-  // const addUserRepository: IAddUserRepository = new AddUserRepository()
-  // const addUserService: IAddUserUseCase = new AddUserService(addUserRepository)
-  // const user = await addUserService.execute({ name: 'leandro', email: 'leandro@gmail.com', password: 'senhashow' })
+  const addUserRepository: IAddUserRepository = new AddUserRepository()
   const loadUserByEmailRepository: ILoadUserByEmailRepository = new LoadUserByEmailRepository()
-  const loadUserByEmailService: ILoadUserByEmailUseCase = new LoadUserByEmailService(loadUserByEmailRepository)
-  const user = await loadUserByEmailService.execute('leandro@dgmail.com')
+  const addUserService: IAddUserUseCase = new AddUserService(addUserRepository, new LoadUserByEmailService(loadUserByEmailRepository))
+  const user = await addUserService.execute({ name: 'leandro', email: 'leansdro@gmail.com', password: 'senhashow' })
+
+  // const loadUserByEmailRepository: ILoadUserByEmailRepository = new LoadUserByEmailRepository()
+  // const loadUserByEmailService: ILoadUserByEmailUseCase = new LoadUserByEmailService(loadUserByEmailRepository)
+  // const user = await loadUserByEmailService.execute('leandro@dgmail.com')
   console.log(user)
   console.log('APP initiaadasadzed')
 }).catch(error => {
