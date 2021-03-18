4552ee3ed23e77f5a54e5ce4190f5b6e5d20fc42
leandro0101 <leandrovieira3306@gmail.com>
2021-03-18 20:02:51 -0300

    feat: add password comparation

--- src/main/factories/controllers/user/validations.ts
+++ src/main/factories/controllers/user/validations.ts
@@ -1,12 +1,14 @@
 import { EmailValidatorAdapter } from '@infra/validators/email-validator-adapter'
 import { IValidation } from '@presentation/protocols'
 import { ValidationComposite, RequiredParamValidation, EmailValidation } from '@validations/validators'
+import { ComparePasswordsValidation } from '@/src/validations/validators/compare-password-validation'
 
 export const makeAddUserValidations = (): ValidationComposite => {
   const validations: IValidation[] = []
-  for (const field of ['name', 'email', 'password']) {
+  for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
     validations.push(new RequiredParamValidation(field))
   }
+  validations.push(new ComparePasswordsValidation('password', 'passwordConfirmation'))
   validations.push(new EmailValidation(new EmailValidatorAdapter(), 'email'))
   return new ValidationComposite(validations)
 }
---
+++ src/validations/validators/compare-password-validation.ts
@@ -0,0 +1,11 @@
+import { IValidation } from '@presentation/protocols'
+import { InvalidParamError } from '@presentation/errors'
+
+export class ComparePasswordsValidation implements IValidation {
+  constructor (private readonly fieldName: string, private readonly fieldToCompareName: string) {}
+  validate (input: any): Error {
+    if (input[this.fieldName] !== input[this.fieldToCompareName]) {
+      return new InvalidParamError(this.fieldToCompareName)
+    }
+  }
+}
