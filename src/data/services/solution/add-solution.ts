ac25d8779d4a6e9f1d28a71a89574d8cea45489d
leandro0101 <leandrovieira3306@gmail.com>
2021-03-23 19:17:25 -0300

    refactor: rename variables at AddSolutionService

--- src/data/services/solution/add-solution.ts
+++ src/data/services/solution/add-solution.ts
@@ -1,18 +1,20 @@
 import { IAddSolutionUseCase, ICreateSolutionModel, ILoadOneProblemAndUserUseCase } from '@domain/usecases'
-import { ICreateSolutionDTO, IReturnSolutionDTO } from '../../dtos'
+import { IReturnSolutionDTO } from '../../dtos'
 import { IAddSolutionToProblemRepository, IAddSolutionToUserRepository } from '../../repositories'
 
 export class AddSolutionService implements IAddSolutionUseCase {
-  constructor (private readonly loadOneProblemAndUserUseCase: ILoadOneProblemAndUserUseCase,
+  constructor (private readonly loadOneProblemAndUserService: ILoadOneProblemAndUserUseCase,
     private readonly addSolutionToProblemRepository: IAddSolutionToProblemRepository,
     private readonly addSolutionToUserRepository: IAddSolutionToUserRepository) {}
 
   async execute (createSolutionData: ICreateSolutionModel): Promise<IReturnSolutionDTO> {
     const { userId, problemId, description, sourceCode } = createSolutionData
-    const { user, problem } = await this.loadOneProblemAndUserUseCase.execute(userId, problemId)
-    const { created_at, id, stars } = await this.addSolutionToProblemRepository
-    .execute({ description, sourceCode }, problem)
+    const { user, problem } = await this.loadOneProblemAndUserService.execute(userId, problemId)
+    const { user: returnedUser, problem: returnedProblem, ...solution } = await this.
+    addSolutionToProblemRepository.execute({ description, sourceCode }, problem)
+
     await this.addSolutionToUserRepository.execute({ description, sourceCode }, user)
-    return { id, sourceCode, stars, created_at, description } 
+
+    return solution
   }
 }
