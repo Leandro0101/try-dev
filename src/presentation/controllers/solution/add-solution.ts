aea812e28c1a1f2aa425d3b641dc16508b846645
leandro0101 <leandrovieira3306@gmail.com>
2021-03-23 22:04:56 -0300

    feat: add AddSolutionController

---
+++ src/presentation/controllers/solution/add-solution.ts
@@ -0,0 +1,15 @@
+import { IAddSolutionUseCase } from '@domain/usecases'
+import { ok } from '../../helpers/http'
+import { IController, IHttpRequest, IHttpResponse } from '../../protocols'
+
+export class AddSolutionController implements IController {
+  constructor (private readonly addSolutionService: IAddSolutionUseCase) {}
+  async handle (httpRequest: IHttpRequest): Promise<IHttpResponse> {
+    const { userId } = httpRequest.params
+    const { problemId } = httpRequest.query
+    const { sourceCode, description } = httpRequest.body
+    const solution = await this.addSolutionService.execute({ userId, problemId, description, sourceCode })
+
+    return ok(solution)
+  }
+}
