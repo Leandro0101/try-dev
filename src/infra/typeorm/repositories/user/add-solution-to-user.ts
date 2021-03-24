334861d42af64425d11f352a7d25c2d55f120227
leandro0101 <leandrovieira3306@gmail.com>
2021-03-23 21:04:54 -0300

    feat: add AddSolutionToUserRepository at infra layer

---
+++ src/infra/typeorm/repositories/user/add-solution-to-user.ts
@@ -0,0 +1,12 @@
+import { ISolutionEntity, IUserEntity } from '@domain/entities'
+import { IAddSolutionToUserRepository } from '@data/repositories'
+import { BaseUserRepository } from '../base-user-repository'
+import { getCustomRepository } from 'typeorm'
+
+export class AddSolutionToUserRepository implements IAddSolutionToUserRepository {
+  async execute (solution: ISolutionEntity, user: IUserEntity): Promise<void> {
+    const baseRepository = getCustomRepository(BaseUserRepository)
+    user.solutions.push(solution)
+    await baseRepository.save(user)
+  }
+}
