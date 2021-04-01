81c1d8f9870f97040fd363e6012a0d2711c9f4b2
leandro0101 <leandrovieira3306@gmail.com>
2021-03-31 22:38:18 -0300

    feat: add data limit at LoadUserByIdRepository at infra layer

--- src/data/services/user/load-user-by-id.ts
+++ src/data/services/user/load-user-by-id.ts
@@ -10,6 +10,20 @@ export class LoadUserByIdService implements ILoadUserByIdUseCase {
 
     if (!foundUser) return null
 
-    return foundUser
+    const { solutions, problems, stars } = foundUser
+    const TAKE = 15
+    const INIT = 0
+    const solutionsWithLimitedSize = solutions.slice(INIT, TAKE)
+    const problemsWithLimitedSize = problems.slice(INIT, TAKE)
+    const starsWithLimitedSize = stars.slice(INIT, TAKE)
+
+    const user = Object.assign(foundUser,
+      {
+        problems: problemsWithLimitedSize,
+        solutions: solutionsWithLimitedSize,
+        stars: starsWithLimitedSize
+      })
+
+    return user
   }
 }
--- src/infra/typeorm/repositories/user/load-user-by-id.ts
+++ src/infra/typeorm/repositories/user/load-user-by-id.ts
@@ -1,13 +1,28 @@
 import { ILoadUserByIdRepository } from '@data/repositories'
 import { IUserEntity } from '@domain/entities'
 import { getCustomRepository } from 'typeorm'
+import { BaseProblemRepository } from '../base-problem-repository'
+import { BaseSolutionRepository } from '../base-solution-repository'
+import { BaseStarRepository } from '../base-star-repository'
 import { BaseUserRepository } from '../base-user-repository'
 
 export class LoadUserByIdRepository implements ILoadUserByIdRepository {
   async execute (id: string): Promise<IUserEntity> {
-    const baseRepository = getCustomRepository(BaseUserRepository)
-    const foundUser = await baseRepository.find({ where: { id }, relations: ['problems', 'stars', 'solutions'] })
+    const baseUserRepository = getCustomRepository(BaseUserRepository)
+    const baseSolutionRepository = getCustomRepository(BaseSolutionRepository)
+    const baseProblemRepository = getCustomRepository(BaseProblemRepository)
+    const baseStarRepository = getCustomRepository(BaseStarRepository)
 
-    return foundUser[0]
+    const TAKE = 20
+    const foundUser = await baseUserRepository.findOne(id)
+    const stars = await baseStarRepository.find({ where: { user: id }, take: TAKE })
+    const problems = await baseProblemRepository.find({ where: { user: id }, take: TAKE })
+    const solutions = await baseSolutionRepository.find({ where: { user: id }, take: TAKE })
+
+    foundUser.solutions = solutions
+    foundUser.problems = problems
+    foundUser.stars = stars
+
+    return foundUser
   }
 }
