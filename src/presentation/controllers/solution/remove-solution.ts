5705f771bb6563aec33ec83689873d35222167d4
leandro0101 <leandrovieira3306@gmail.com>
2021-04-06 20:12:38 -0300

    feat: catch internal errors

--- src/presentation/controllers/solution/remove-solution.ts
+++ src/presentation/controllers/solution/remove-solution.ts
@@ -1,5 +1,5 @@
 import { IController, IHttpRequest, IHttpResponse, IValidation } from '../../protocols'
-import { badRequest, forbidden, ok } from '../../helpers/http'
+import { badRequest, forbidden, ok, serverError } from '../../helpers/http'
 import { IRemoveSolutionUseCase } from '@domain/usecases'
 import { ResourceNotFoundError } from '../../errors'
 
@@ -10,15 +10,19 @@ export class RemoveSolutionController implements IController {
   ) {}
 
   async handle (httpRequest: IHttpRequest): Promise<IHttpResponse> {
-    const { solutionId } = httpRequest.params
-    const error = this.validation.validate({ solutionId })
+    try {
+      const { solutionId } = httpRequest.params
+      const error = this.validation.validate({ solutionId })
 
-    if (error) return badRequest(error)
+      if (error) return badRequest(error)
 
-    const response = await this.removeSolutionService.execute(solutionId)
+      const response = await this.removeSolutionService.execute(solutionId)
 
-    if (response === null) return forbidden(new ResourceNotFoundError('solution'))
+      if (response === null) return forbidden(new ResourceNotFoundError('solution'))
 
-    return ok()
+      return ok()
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
