beea427a863b5102bd16a2cdbe24fba5a02feca6
leandro0101 <leandrovieira3306@gmail.com>
2021-06-02 17:57:05 -0300

    refactor: add factories

--- src/infra/typeorm/seeds/index.ts
+++ src/infra/typeorm/seeds/index.ts
@@ -1,8 +1,10 @@
 import { getCustomRepository } from 'typeorm'
 import { BaseUserRepository } from '../repositories/base-user-repository'
 import { BaseProblemRepository } from '../repositories/base-problem-repository'
-import { problemsFactory, solutionsFactory, usersFactory } from './factories'
+import { problemsFactory, solutionsFactory, starsFactory, usersFactory } from './factories'
 import { BaseSolutionRepository } from '../repositories/base-solution-repository'
+import { BaseStarRepository } from '../repositories/base-star-repository'
+
 export const seeds = new Map()
 
 seeds.set('users', async () => {
@@ -18,4 +20,9 @@ seeds.set('problems', async () => {
 seeds.set('solutions', async () => {
   const baseRepository = getCustomRepository(BaseSolutionRepository)
   await baseRepository.save(await solutionsFactory())
+})
+
+seeds.set('stars', async () => {
+  const baseRepository = getCustomRepository(BaseStarRepository)
+  await baseRepository.save(await starsFactory())
 })
--- src/main/server.ts
+++ src/main/server.ts
@@ -2,14 +2,15 @@ import 'reflect-metadata'
 import { connect } from '@infra/typeorm/helpers/typeorm'
 import app from './config/app'
 import { getConnection } from 'typeorm'
-import { seeds } from '@infra/typeorm/seeds'
+import { seeds } from '../infra/typeorm/seeds'
 connect().then(async () => {
   app.listen(3000, () => console.log('RODAND10O'))
   await getConnection().runMigrations()
-  // await seeds.get('users')()
-  // await seeds.get('problems')()
-  // await seeds.get('solutions')()
+  await seeds.get('users')()
+  await seeds.get('problems')()
+  await seeds.get('solutions')()
+  await seeds.get('stars')()
 }).catch(error => {
-  console.log('OCORREU Um ERR0')
+  console.log('OCORREU Um ERROOR')
   console.log(error)
 })
