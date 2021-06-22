6e0281cf46f8a00cae2829957a159e2d5ca47b9f
leandro0101 <leandrovieira3306@gmail.com>
2021-06-22 10:30:55 -0300

    ci: add multistage build

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,14 @@
-FROM node:14
+FROM node:14-alpine AS appbuild
 
 WORKDIR /home/trydev_api
+COPY package.json ./
+COPY yarn.lock ./
+COPY ormconfig.js ./
+RUN yarn install
+COPY ./dist ./dist
 
-COPY package.json .
-COPY yarn.lock .
-COPY dist .
+FROM node:14-alpine
 
-RUN yarn install --prod
-
+WORKDIR /home/trydev_api
+COPY --from=appbuild /home/trydev_api/ .
 CMD yarn start
--- src/infra/typeorm/repositories/solution/most-popular-solutions.ts
+++ src/infra/typeorm/repositories/solution/most-popular-solutions.ts
@@ -10,7 +10,7 @@ export class MostPopularSolutionsRepository implements IMostPopularSolutionsRepo
       SELECT s.id, s.description, s."sourceCode", 
       CAST ( AVG (st.value) AS NUMERIC( 10,2 ) ) as average, 
       COUNT ( st.id ) AS "starsQuantity", s."createdAt"
-      FROM solutions s RIGHT OUTER JOIN stars st ON (s.id = st."solutionId") 
+      FROM solutions s LEFT OUTER JOIN stars st ON (s.id = st."solutionId") 
       WHERE s."problemId"='${problemId}'
       GROUP BY (s.id) ORDER BY ("starsQuantity") DESC
       OFFSET ${take * (skip - 1)} LIMIT ${take}
