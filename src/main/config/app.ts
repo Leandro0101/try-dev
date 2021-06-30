45a44f491902e9678034fee279275a8ff6568a8c
leandro0101 <leandrovieira3306@gmail.com>
2021-06-30 16:08:04 -0300

    feat: add headers config

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -2,9 +2,11 @@ import { EndPointNotFoundMiddleware } from '@presentation/middlewares/endpoint-n
 import 'dotenv/config'
 import express from 'express'
 import { adaptMiddleware } from '../adapters/express-middleware'
+import { middlewares } from './middlewares'
 import setupRoutes from './routes'
 const app = express()
 app.use(express.json())
+middlewares(app)
 setupRoutes(app)
 app.use('*', adaptMiddleware(new EndPointNotFoundMiddleware()))
 export default app
